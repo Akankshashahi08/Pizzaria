aa05ea1e6f9c01bdd922d8c7080b0d18e7f134d7
Akanksha Shahi <priets.shahi@gmail.com>
2022-07-22 01:19:09 +0530

    API exception

--- Pizzaria.Function.Api/Controller/OrderController.cs
+++ Pizzaria.Function.Api/Controller/OrderController.cs
@@ -5,7 +5,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Pizzaria.Entities.DataModels;
 using Pizzaria.Function.Api.Processor.Interface;
+using System;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace Pizzaria.Function.Api.Controller
 {
@@ -25,24 +27,51 @@ namespace Pizzaria.Function.Api.Controller
         public async Task<IActionResult> GetOrderByIdAsync(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "order/{orderId}")] HttpRequest req, int orderId)
         {
-            this.logger.LogInformation($"Get Order By {orderId} request received");
+            this.logger.LogInformation($"{nameof(OrderController)} => GetOrderByIdAsync : Starts");
 
-            var order = await this.orderProcessor.GetOrderByIdAsync(orderId).ConfigureAwait(false);
+            try
+            {
+                this.logger.LogInformation($"Get Order By {orderId} request received");
 
-            return new OkObjectResult(order);
+                var order = await this.orderProcessor.GetOrderByIdAsync(orderId).ConfigureAwait(false);
+
+                return new OkObjectResult(order);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"{nameof(OrderController)} => GetOrderByIdAsync : Exception Occured   : {ex.Message}");
+                return new InternalServerErrorResult();
+            }
+            finally
+            {
+                this.logger.LogInformation($"{nameof(OrderController)} => GetOrderByIdAsync : Ends");
+            }
         }
 
         [FunctionName("saveOrder")]
         public async Task<IActionResult> SaveOrderAsync(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "order/save")] HttpRequest req)
         {
-            this.logger.LogInformation("Save Order request received.");
+            this.logger.LogInformation($"{nameof(OrderController)} => SaveOrderAsync : Starts");
+            try
+            {
+                this.logger.LogInformation("Save Order request received.");
 
-            var data = await req.GetObjectAsync<Order>().ConfigureAwait(false);
+                var data = await req.GetObjectAsync<Order>().ConfigureAwait(false);
 
-            var newOrder = await this.orderProcessor.SaveOrderAsync(data).ConfigureAwait(false);
+                var newOrder = await this.orderProcessor.SaveOrderAsync(data).ConfigureAwait(false);
 
-            return new OkObjectResult(newOrder);
+                return new OkObjectResult(newOrder);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"{nameof(OrderController)} => SaveOrderAsync : Exception Occured   : {ex.Message}");
+                return new InternalServerErrorResult();
+            }
+            finally
+            {
+                this.logger.LogInformation($"{nameof(OrderController)} => SaveOrderAsync : Ends");
+            }
         }
     }
 }
--- Pizzaria.Function.Api/Controller/ProductController.cs
+++ Pizzaria.Function.Api/Controller/ProductController.cs
@@ -4,7 +4,9 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Pizzaria.Function.Api.Processor.Interface;
+using System;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace Pizzaria.Function.Api.Controller
 {
@@ -23,66 +25,144 @@ namespace Pizzaria.Function.Api.Controller
         public async Task<IActionResult> GetProductByIdAsync(
            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "product/{productId}")] HttpRequest req, int productId)
         {
-            this.logger.LogInformation($"Get Product By {productId} request received");
+            this.logger.LogInformation($"{nameof(ProductController)} => GetProductByIdAsync : Starts");
+            try
+            {
+                this.logger.LogInformation($"Get Product By {productId} request received");
 
-            var product = await this.productProcessor.GetProductByIdAsync(productId).ConfigureAwait(false);
+                var product = await this.productProcessor.GetProductByIdAsync(productId).ConfigureAwait(false);
 
-            return new OkObjectResult(product);
+                return new OkObjectResult(product);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"{nameof(ProductController)} => GetProductByIdAsync : Exception Occured   : {ex.Message}");
+                return new InternalServerErrorResult();
+            }
+            finally
+            {
+                this.logger.LogInformation($"{nameof(ProductController)} => GetProductByIdAsync : Ends");
+            }
         }
 
         [FunctionName("menu")]
         public async Task<IActionResult> GetMenuAsync(
            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "menu/all")] HttpRequest req)
         {
-            this.logger.LogInformation($"Get All Product request received");
+            this.logger.LogInformation($"{nameof(ProductController)} => GetMenuAsync : Starts");
+            try
+            {
+                this.logger.LogInformation($"Get All Product request received");
 
-            var products = await this.productProcessor.GetMenuAsync().ConfigureAwait(false);
+                var products = await this.productProcessor.GetMenuAsync().ConfigureAwait(false);
 
-            return new OkObjectResult(products);
+                return new OkObjectResult(products);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"{nameof(ProductController)} => GetMenuAsync : Exception Occured   : {ex.Message}");
+                return new InternalServerErrorResult();
+            }
+            finally
+            {
+                this.logger.LogInformation($"{nameof(ProductController)} => GetMenuAsync : Ends");
+            }
         }
 
         [FunctionName("allCategories")]
         public async Task<IActionResult> GetAllCategoriesAsync(
            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "category/all")] HttpRequest req)
         {
-            this.logger.LogInformation($"Get All Categories request received");
+            this.logger.LogInformation($"{nameof(ProductController)} => GetAllCategoriesAsync : Starts");
+            try
+            {
+                this.logger.LogInformation($"Get All Categories request received");
 
-            var categories = await this.productProcessor.GetAllCategoriesAsync().ConfigureAwait(false);
+                var categories = await this.productProcessor.GetAllCategoriesAsync().ConfigureAwait(false);
 
-            return new OkObjectResult(categories);
+                return new OkObjectResult(categories);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"{nameof(ProductController)} => GetAllCategoriesAsync : Exception Occured   : {ex.Message}");
+                return new InternalServerErrorResult();
+            }
+            finally
+            {
+                this.logger.LogInformation($"{nameof(ProductController)} => GetAllCategoriesAsync : Ends");
+            }
         }
 
         [FunctionName("allCrust")]
         public async Task<IActionResult> GetAllCrustAsync(
            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "crust/all")] HttpRequest req)
         {
-            this.logger.LogInformation($"Get All Crusts request received");
+            this.logger.LogInformation($"{nameof(ProductController)} => GetAllCrustAsync : Starts");
+            try
+            {
+                this.logger.LogInformation($"Get All Crusts request received");
 
-            var crusts = await this.productProcessor.GetAllCrustsAsync().ConfigureAwait(false);
+                var crusts = await this.productProcessor.GetAllCrustsAsync().ConfigureAwait(false);
 
-            return new OkObjectResult(crusts);
+                return new OkObjectResult(crusts);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"{nameof(ProductController)} => GetAllCrustAsync : Exception Occured   : {ex.Message}");
+                return new InternalServerErrorResult();
+            }
+            finally
+            {
+                this.logger.LogInformation($"{nameof(ProductController)} => GetAllCrustAsync : Ends");
+            }
         }
 
         [FunctionName("allToppings")]
         public async Task<IActionResult> GetAllToppingsAsync(
            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "topping/all")] HttpRequest req)
         {
-            this.logger.LogInformation($"Get All Toppings request received");
+            this.logger.LogInformation($"{nameof(ProductController)} => GetAllToppingsAsync : Starts");
+            try
+            {
+                this.logger.LogInformation($"Get All Toppings request received");
 
-            var toppings = await this.productProcessor.GetAllToppingsAsync().ConfigureAwait(false);
+                var toppings = await this.productProcessor.GetAllToppingsAsync().ConfigureAwait(false);
 
-            return new OkObjectResult(toppings);
+                return new OkObjectResult(toppings);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"{nameof(ProductController)} => GetAllToppingsAsync : Exception Occured   : {ex.Message}");
+                return new InternalServerErrorResult();
+            }
+            finally
+            {
+                this.logger.LogInformation($"{nameof(ProductController)} => GetAllToppingsAsync : Ends");
+            }
         }
 
         [FunctionName("allSize")]
         public async Task<IActionResult> GetAllSizesAsync(
            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "size/all")] HttpRequest req)
         {
-            this.logger.LogInformation($"Get All Sizes request received");
+            this.logger.LogInformation($"{nameof(ProductController)} => GetAllSizesAsync : Starts");
+            try
+            {
+                this.logger.LogInformation($"Get All Sizes request received");
 
-            var sizes = await this.productProcessor.GetAllSizesAsync().ConfigureAwait(false);
+                var sizes = await this.productProcessor.GetAllSizesAsync().ConfigureAwait(false);
 
-            return new OkObjectResult(sizes);
+                return new OkObjectResult(sizes);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError($"{nameof(ProductController)} => GetAllSizesAsync : Exception Occured   : {ex.Message}");
+                return new InternalServerErrorResult();
+            }
+            finally
+            {
+                this.logger.LogInformation($"{nameof(ProductController)} => GetAllSizesAsync : Ends");
+            }
         }
     }
 }
